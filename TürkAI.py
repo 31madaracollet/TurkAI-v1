3b0a56c8436780240b54260b353ea8c434adc614
Sucuk9977 <akif97736@gmail.com>
2026-01-18 20:03:31 +0300

    Update TürkAI.py

--- TürkAI.py
+++ TürkAI.py
@@ -1,6 +1,3 @@
-# v67.1 - GitHub Pages Uyum Yaması
-st.sidebar.info(f"🚀 Sunucu: GitHub Pages üzerinden bağlı")
-st.sidebar.caption("Versiyon: Pro v67.1 | Statü: Aktif")
 import streamlit as st
 import requests
 from bs4 import BeautifulSoup
@@ -163,6 +160,7 @@ if msg:
                     st.rerun()
             st.warning("Sonuç bulunamadı.")
         except: st.error("Sunucu hatası!")
+
 
 
 
